4655b92ec5a7b48796c7859064480e676351ea36
Bevoniezh <ronan.lepuil@gmail.com>
2025-10-28 07:52:43 +0100

    Update port

--- MassLearn 2.2.py
+++ MassLearn 2.2.py
@@ -156,5 +156,5 @@ def display_page(pathname):
         return '404'
     
 if __name__ == '__main__':
-    app.run(debug=False)         
+    app.run(host='127.0.0.1', port=8060, debug=False)      
 
